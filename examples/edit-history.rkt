fc8d62618c42e3e7c8ee507efb1944caa88d9044
yjqww6 <343519265@qq.com>
2025-01-14 02:01:57 +0800

    change control

--- examples/edit-history.rkt
+++ examples/edit-history.rkt
@@ -3,62 +3,97 @@
 (provide edit-history!)
 
 (define (edit-history!)
-  (current-history
-   (edit-history (current-history))))
+  (cond
+    [(edit-history (current-history))
+     =>
+     current-history]))
 
 (define (edit-history initial-data)
-  (define dialog (new dialog% [label "History Editor"] [width 400] [height 300]
-                      [style '(resize-border)]))
+  (define dialog (new dialog% [label "History Editor"] [width 480] [height 640]
+                      [style '(resize-border close-button)]))
 
-  (define items (list))
+  (define items initial-data)
+  
+  (define (update-list-box)
+    (send list-box set
+          (for/list ([h (in-list items)])
+            (hash-ref h 'role))
+          (for/list ([h (in-list items)])
+            (define str (hash-ref h 'content))
+            (cond
+              [(label-string? str) str]
+              [else
+               (string-append (substring str 0 197) "...")]))))
 
-  (define (make-item-panel parent role content)
-    (define main-panel (new vertical-panel% [parent parent] 
-                            [alignment '(left center)] 
-                            [border 2] 
-                            [spacing 5]
-                            [style '(border)]))
-  
-    (define top-panel (new horizontal-panel% [parent main-panel] [alignment '(left center)] [spacing 5]))
-    (define role-field (new text-field% [parent top-panel] [label "Role: "] [init-value role]))
-    (define delete-button (new button% [parent top-panel] [label "Delete"]
-                               [callback (λ (b e) (delete-item main-panel))]))
-  
-    (define content-field (new text-field% [parent main-panel] [label ""]
-                               [style '(multiple)] [min-width 200] [min-height 50] [init-value content]))
-  
-    (values main-panel role-field content-field))
+  (define list-box (new list-box% [parent dialog] [label "History"]
+                        [style '(single column-headers)]
+                        [choices '()]
+                        [columns (list "Role" "Content")]
+                        [callback (λ (b e) (on-selection-change))]))
+
+  (define role-field (new text-field% [parent dialog] [label "Role: "] [init-value "user"]))
+  (define content-field
+    (new text-field%
+         [parent dialog]
+         [label "Content: "]
+         [init-value ""]
+         [style '(multiple)]
+         [stretchable-height #f]))
+
+  (define (on-selection-change)
+    (define selected-index (send list-box get-selection))
+    (when (and selected-index (<= 0 selected-index (sub1 (length items))))
+      (define selected-item (list-ref items selected-index))
+      (send role-field set-value (hash-ref selected-item 'role))
+      (send content-field set-value (hash-ref selected-item 'content))))
 
   (define (add-item)
-    (define last-role (if (null? items)
-                          "user"
-                          (send (cadr (last items)) get-value)))
-    (define new-role (if (equal? last-role "assistant") "user" "assistant"))
-    (define-values (main-panel role-field content-field) (make-item-panel item-list new-role ""))
-    (set! items (append items (list (list main-panel role-field content-field)))))
+    (define role (cond
+                   [(null? items) "user"]
+                   [(string=? "user" (hash-ref (last items) 'role))
+                    "assistant"]
+                   [else "user"]))
+    (set! items (append items (list (hasheq 'role role 'content ""))))
+    (update-list-box)
+    (send list-box set-selection (- (length items) 1))
+    (on-selection-change))
 
-  (define (delete-item main-panel)
-    (send item-list delete-child main-panel)
-    (set! items (filter (λ (item) (not (eq? (car item) main-panel))) items)))
+  (define (delete-item)
+    (define selected-index (send list-box get-selection))
+    (when (and selected-index (<= 0 selected-index (sub1 (length items))))
+      (set! items (append (take items selected-index) (drop items (add1 selected-index))))
+      (update-list-box)
+      (send list-box set-selection
+            (if (= selected-index (length items))
+                (sub1 selected-index)
+                selected-index))
+      (on-selection-change)))
 
-  (define item-list (new vertical-panel% [parent (new vertical-panel% [parent dialog])]))
+  (define (save-item)
+    (define selected-index (send list-box get-selection))
+    (when (and selected-index (<= 0 selected-index (sub1 (length items))))
+      (define role (send role-field get-value))
+      (define content (send content-field get-value))
+      (set! items (list-set items selected-index
+                            (hasheq 'role role 'content content)))
+      (update-list-box)
+      (send list-box set-selection selected-index)
+      (on-selection-change)))
 
-  (for-each (λ (item)
-              (define-values (main-panel role-field content-field) (make-item-panel item-list (hash-ref item 'role) (hash-ref item 'content)))
-              (set! items (append items (list (list main-panel role-field content-field)))))
-            initial-data)
+  (define h-panel (new horizontal-pane% [parent dialog] [stretchable-height #f]))
 
-  (define add-button (new button% [parent dialog] [label "Add Item"] [callback (λ (b e) (add-item))]))
+  (define add-button (new button% [parent h-panel] [label "Add Item"] [callback (λ (b e) (add-item))]))
+  (define delete-button (new button% [parent h-panel] [label "Delete Item"] [callback (λ (b e) (delete-item))]))
+  (define save-button (new button% [parent h-panel] [label "Save Item"] [callback (λ (b e) (save-item))]))
 
-  (define (get-edited-items)
-    (map (λ (item)
-           (hasheq 'role (send (cadr item) get-value) 'content (send (caddr item) get-value)))
-         items))
-
+  (define ok? #f)
   (define ok-button (new button% [parent dialog] [label "OK"]
-                         [callback (λ (b e) 
+                         [callback (λ (b e)
+                                     (set! ok? #t)
                                      (send dialog show #f))]))
+  (update-list-box)
 
   (send dialog show #t)
-
-  (get-edited-items))
+  (if ok?
+      items
+      #f))
